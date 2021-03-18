531124d509c5a0987a7da42ea58dcede4c711649
kalyssao <kalyssaowusu@gmail.com>
2021-03-18 16:45:26 +0000

    structure to persist to database

---
+++ src/main/java/com/example/exchangeconnectivity/exchange/ExchangeOrderService.java
@@ -0,0 +1,13 @@
+package com.example.exchangeconnectivity.exchange;
+
+import org.springframework.web.client.RestTemplate;
+
+public class ExchangeOrderService {
+
+    public void persistToDb(ExchangeOrder exchangeOrder){
+        final String API_URL = "https://tradeenginedb.herokuapp.com/api/v1/exchangeorder";
+        RestTemplate restTemplate = new RestTemplate();
+
+        restTemplate.postForObject(API_URL, exchangeOrder, ExchangeOrder.class);
+    }
+}
