e12ac52bf43cf014b65bc9a5cbe0632a427efd45
kalyssao <kalyssaowusu@gmail.com>
2021-03-19 19:10:49 +0000

    testing with localhost

--- src/main/java/com/example/exchangeconnectivity/exchange/ExchangeOrderService.java
+++ src/main/java/com/example/exchangeconnectivity/exchange/ExchangeOrderService.java
@@ -6,6 +6,7 @@ public class ExchangeOrderService {
 
     public void persistToDb(ExchangeOrder exchangeOrder){
         final String API_URL = "https://tradeenginedb.herokuapp.com/api/v1/exchangeorder";
+        // final String API_URL = "http://localhost:8080/api/v1/exchangeorder";
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.setErrorHandler(new ExchangeOrderServiceErrorHandler());
 
