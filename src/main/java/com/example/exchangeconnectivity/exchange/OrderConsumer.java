adadf3f0a2ee9c606505f21610cc1400666fdfc7
kalyssao <kalyssaowusu@gmail.com>
2021-03-19 16:12:28 +0000

    everything conforming so far

--- src/main/java/com/example/exchangeconnectivity/exchange/OrderConsumer.java
+++ src/main/java/com/example/exchangeconnectivity/exchange/OrderConsumer.java
@@ -14,23 +14,33 @@ public class OrderConsumer {
         List<String> orders;
 
         while(true){
-            System.out.println("waiting for incoming order");
+            System.out.println("waiting for order");
             orders = jedis.blpop(0, "incoming-orders");
             String order = orders.get(1);
+
+            System.out.println("order from engine " + order);
 
             ObjectMapper mapper = new ObjectMapper();
             ExchangeOrder exchangeOrder = mapper.readValue(order, ExchangeOrder.class);
 
+            System.out.println(exchangeOrder);
+
             ExchangeService exchangeService = new ExchangeService();
             String orderId = exchangeService.confirmOrderWithExchange(exchangeOrder);
 
+            System.out.println(orderId);
+
             String sansQuotes = orderId.replaceAll("^\"|\"$", "");
             exchangeOrder.setId(sansQuotes);
-            exchangeOrder.setStatus("placed");
+            exchangeOrder.setStatus("Pending");
+
+            System.out.println("final exchange order" + exchangeOrder);
+//
+//            System.out.println("persist this" + exchangeOrder);
 
             // persist to database through rest call
-            ExchangeOrderService exchangeOrderService = new ExchangeOrderService();
-            exchangeOrderService.persistToDb(exchangeOrder);
+//            ExchangeOrderService exchangeOrderService = new ExchangeOrderService();
+//            exchangeOrderService.persistToDb(exchangeOrder);
 
          }
     }
