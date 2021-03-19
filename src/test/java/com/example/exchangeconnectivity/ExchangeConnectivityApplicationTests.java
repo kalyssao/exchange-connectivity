3826220477dcf16c4aa596cd0120e122c120e7b3
kalyssao <kalyssaowusu@gmail.com>
2021-03-19 14:35:55 +0000

    test dependencies

--- src/test/java/com/example/exchangeconnectivity/ExchangeConnectivityApplicationTests.java
+++ src/test/java/com/example/exchangeconnectivity/ExchangeConnectivityApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ExchangeConnectivityApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
