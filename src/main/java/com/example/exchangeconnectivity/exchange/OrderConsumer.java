b352694a22f040dcd6cd8f50beeaa71c775aebd8
kalyssao <kalyssaowusu@gmail.com>
2021-03-19 14:37:40 +0000

    stripped extra quotes, set id, set status

--- src/main/java/com/example/exchangeconnectivity/exchange/OrderConsumer.java
+++ src/main/java/com/example/exchangeconnectivity/exchange/OrderConsumer.java
@@ -1,5 +1,6 @@
 package com.example.exchangeconnectivity.exchange;
 
+import java.net.URI;
 import java.util.List;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -8,38 +9,28 @@ import redis.clients.jedis.Jedis;
 
 public class OrderConsumer {
     public void consumeOrder() throws JsonProcessingException {
-         Jedis jedis = new Jedis("localhost", 8083, 1800);
-         Jedis jedis1 = new Jedis("localhost", 8083, 1800);
-         List<String> orders;
+        Jedis jedis = new Jedis("redis-17587.c92.us-east-1-3.ec2.cloud.redislabs.com", 17587);
+        jedis.auth("rLAKmB4fpXsRZEv9eJBkbddhTYc1RWtK");
+        List<String> orders;
 
-         while(true){
-             System.out.println("waiting for incoming order");
-             orders = jedis.blpop(0, "incoming-orders");
-             String order = orders.get(1);
+        while(true){
+            System.out.println("waiting for incoming order");
+            orders = jedis.blpop(0, "incoming-orders");
+            String order = orders.get(1);
 
-             ObjectMapper mapper = new ObjectMapper();
-             ExchangeOrder exchangeOrder = mapper.readValue(order, ExchangeOrder.class);
+            ObjectMapper mapper = new ObjectMapper();
+            ExchangeOrder exchangeOrder = mapper.readValue(order, ExchangeOrder.class);
 
-             ExchangeService exchangeService = new ExchangeService();
-             String orderId = exchangeService.confirmOrderWithExchange(exchangeOrder);
-             System.out.println("api response "+ orderId);
+            ExchangeService exchangeService = new ExchangeService();
+            String orderId = exchangeService.confirmOrderWithExchange(exchangeOrder);
 
-             // updated order with string from exchange
-             ExchangeOrder outgoingOrder = new ExchangeOrder(orderId,
-                     exchangeOrder.getProduct(), exchangeOrder.getQuantity(),
-                     exchangeOrder.getPrice(), exchangeOrder.getSide(), exchangeOrder.getExchange());
-             System.out.println(outgoingOrder);
+            String sansQuotes = orderId.replaceAll("^\"|\"$", "");
+            exchangeOrder.setId(sansQuotes);
+            exchangeOrder.setStatus("placed");
 
-             // not seeing id in this json object
-             // String outgoingOrderJson = mapper.writeValueAsString(outgoingOrder);
-             // System.out.println(outgoingOrderJson);
-
-             // persist to database through rest call
-             ExchangeOrderService exchangeOrderService = new ExchangeOrderService();
-             exchangeOrderService.persistToDb(outgoingOrder);
-
-             // need to figure out where persisting will occur
-             //jedis.rpush("outgoing-orders", outgoingOrderJson);
+            // persist to database through rest call
+            ExchangeOrderService exchangeOrderService = new ExchangeOrderService();
+            exchangeOrderService.persistToDb(exchangeOrder);
 
          }
     }
