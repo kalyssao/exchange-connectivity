8532675c4c021513e73b845afad8e1b6767924e0
kalyssao <kalyssaowusu@gmail.com>
2021-03-24 16:25:42 +0000

    db removed local db & error handling

--- src/main/java/com/example/exchangeconnectivity/exchange/ExchangeOrderService.java
+++ src/main/java/com/example/exchangeconnectivity/exchange/ExchangeOrderService.java
@@ -5,11 +5,9 @@ import org.springframework.web.client.RestTemplate;
 public class ExchangeOrderService {
 
     public void persistToDb(ExchangeOrder exchangeOrder){
-        final String API_URL = "https://tradeenginedb.herokuapp.com/api/v1/exchangeorder";
-        // final String API_URL = "http://localhost:8080/api/v1/exchangeorder";
+        final String API_URL = "https://tradeenginedb.herokuapp.com/api/v1/exchangeorder/new";
         RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setErrorHandler(new ExchangeOrderServiceErrorHandler());
 
-        restTemplate.postForObject(API_URL, exchangeOrder, ExchangeOrder.class);
+        restTemplate.postForObject(API_URL, exchangeOrder, String.class);
     }
 }
