95e35111421bec56b913807671ebc815c9ef1fb2
kalyssao <kalyssaowusu@gmail.com>
2021-03-29 09:56:36 +0000

    removed while loop

--- src/main/java/com/example/exchangeconnectivity/exchange/OrderConsumer.java
+++ src/main/java/com/example/exchangeconnectivity/exchange/OrderConsumer.java
@@ -1,9 +1,7 @@
 package com.example.exchangeconnectivity.exchange;
 
-import java.time.LocalDate;
 import java.util.List;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.client.HttpClientErrorException;
@@ -21,57 +19,55 @@ public class OrderConsumer {
         jedis.auth("rLAKmB4fpXsRZEv9eJBkbddhTYc1RWtK");
         jedis1.auth("rLAKmB4fpXsRZEv9eJBkbddhTYc1RWtK");
 
-        while(true){
-            System.out.println("waiting for incoming order");
-            orders = jedis.blpop(0, "incoming-orders");
+        System.out.println("waiting for incoming order");
+        orders = jedis.blpop(0, "incoming-orders");
 
-            String order = orders.get(1);
+        String order = orders.get(1);
 
-            ObjectMapper mapper = new ObjectMapper();
-            ExchangeOrder exchangeOrder = mapper.readValue(order, ExchangeOrder.class);
+        ObjectMapper mapper = new ObjectMapper();
+        ExchangeOrder exchangeOrder = mapper.readValue(order, ExchangeOrder.class);
 
-            ExchangeService exchangeService = new ExchangeService();
+        ExchangeService exchangeService = new ExchangeService();
 
-            try {
-                orderId = exchangeService.confirmOrderWithExchange(exchangeOrder);
-            } catch (Exception e) {
+        try {
+            orderId = exchangeService.confirmOrderWithExchange(exchangeOrder);
+        } catch (Exception e) {
 
-                // tells the reporting service that exchange said no
-                System.out.println("exchange rejected");
-                exchangeOrder.setStatus("rejected");
+            // tells the reporting service that exchange said no
+            System.out.println("exchange rejected");
+            exchangeOrder.setStatus("rejected");
 
-                jedis1.publish("report-message",
-                        exchangeOrder.toString() + " has been rejected by the exchange");
-
-                exchangeOrderService.persistToDb(exchangeOrder);
-                continue;
-            }
-
-
-            orderId = (orderId.subSequence(1, orderId.length() - 1)).toString();
-
-            // updated order with string from exchange
-            ExchangeOrder outgoingOrder = new ExchangeOrder(orderId,
-                    exchangeOrder.getProduct(), exchangeOrder.getQuantity(),
-                    exchangeOrder.getPrice(), exchangeOrder.getSide(), exchangeOrder.getExchange(),1,"PENDING");
-
-            // persist to database through rest call
-            try {
-                exchangeOrderService.persistToDb(outgoingOrder);
-            } catch (HttpClientErrorException e) {
-                // tell the reporting service the db said no?
-                System.out.println("db");
-//                jedis1.publish("report-message",
-//                        exchangeOrder.toString() + " has been sent to the exchange & persisted into the db");
-
-                continue;
-            }
-
-
-            // publish to messaging service
-            //String message = mapper.writeValueAsString(outgoingOrder);
             jedis1.publish("report-message",
-                    outgoingOrder.toString() + " has been sent to the exchange & persisted into the db");
+                    exchangeOrder.toString() + " has been rejected by the exchange");
+
+            exchangeOrderService.persistToDb(exchangeOrder);
+            return;
         }
+
+
+        orderId = (orderId.subSequence(1, orderId.length() - 1)).toString();
+
+        // updated order with string from exchange
+        ExchangeOrder outgoingOrder = new ExchangeOrder(orderId,
+                exchangeOrder.getProduct(), exchangeOrder.getQuantity(),
+                exchangeOrder.getPrice(), exchangeOrder.getSide(), exchangeOrder.getExchange(),1,"PENDING");
+
+        // persist to database through rest call
+        try {
+            exchangeOrderService.persistToDb(outgoingOrder);
+        } catch (HttpClientErrorException e) {
+            // tell the reporting service the db said no?
+            System.out.println("db");
+                jedis1.publish("report-message",
+                        exchangeOrder.toString() + " has been sent to the exchange & but could not be persisted");
+
+            return;
+        }
+
+
+        // publish to messaging service
+        //String message = mapper.writeValueAsString(outgoingOrder);
+        jedis1.publish("report-message",
+                outgoingOrder.toString() + " has been sent to the exchange & persisted into the db");
     }
 }
